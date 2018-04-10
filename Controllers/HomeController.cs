fe78d4b5a34faf16ea8b2393e0b47ae0f61e8e9f
Christopher <christopher.d.donnelly@gmail.com>
2018-04-09 17:17:56 -0700

    Commit PokeInfo C# assignment

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using PokeInfo.Models;
+
+namespace PokeInfo.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return Redirect("pokemon/1");
+        }
+
+        [HttpGet]
+        [Route("pokemon/{pokeid}")]
+        public IActionResult QueryPoke(int pokeid)
+        {
+            var PokeInfo = new Dictionary<string, object>();
+            
+            WebRequest.GetPokemonDataAsync(pokeid, ApiResponse =>
+                {
+                    PokeInfo = ApiResponse;
+                    List<string> typeList = new List<string>();
+
+                    ViewData["name"] = PokeInfo["name"];
+                    ViewData["height"] = PokeInfo["height"];
+                    ViewData["weight"] = PokeInfo["weight"];
+                    
+                    JArray jArray = PokeInfo["types"] as JArray;
+                    
+                    foreach(JObject item in jArray)
+                    { 
+                        typeList.Add((string)item.GetValue("type")["name"]);
+                    }
+
+                    ViewData["type"] = string.Join(", ", typeList);
+                    ViewData["img"] = ((JObject)PokeInfo["sprites"]).GetValue("front_default");
+                }
+            ).Wait();
+
+            return View();
+        }
+    }
+}
